346ed6916f0fef16fa01fe06c03a7fe46912bc57
Sébastien Andrivet <sebastien.andrivet@scrt.ch>
2016-12-16 19:11:05 +0100

    Translate example, convert big switches to dictionaries, add common OIDs.

--- examples/dump.py
+++ examples/dump.py
@@ -1,22 +1,27 @@
+# -*- coding: utf-8 -*-
 #
 # This file is part of Python-ASN1. Python-ASN1 is free software that is
 # made available under the MIT license. Consult the file "LICENSE" that
 # is distributed together with this file for the exact licensing terms.
 #
-# Python-ASN1 is copyright (c) 2007-2008 by the Python-ASN1 authors. See
+# Python-ASN1 is copyright (c) 2007-2016 by the Python-ASN1 authors. See
 # the file "AUTHORS" for a complete overview.
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import open, bytes, str
 import sys
-import os.path
+import base64
+import binascii
 
 import asn1
 import optparse
 
-def read_pem(input):
+
+def read_pem(input_file):
     """Read PEM formatted input."""
     data = []
     state = 0
-    for line in input:
+    for line in input_file:
         if state == 0:
             if line.startswith('-----BEGIN'):
                 state = 1
@@ -28,104 +33,139 @@ def read_pem(input):
         elif state == 2:
             break
     if state != 2:
-        raise ValueError, 'No PEM encoded input found'
+        raise ValueError('No PEM encoded input found')
     data = ''.join(data)
-    data = data.decode('base64')
-    return data
+    return base64.b64decode(data)
 
-def strid(id):
+
+tag_id_to_string_map = {
+    asn1.Boolean: "BOOLEAN",
+    asn1.Integer: "INTEGER",
+    asn1.BitString: "BIT STRING",
+    asn1.OctetString: "OCTET STRING",
+    asn1.Null: "NULL",
+    asn1.ObjectIdentifier: "OBJECT",
+    asn1.PrintableString: "PRINTABLESTRING",
+    asn1.IA5String: "IA5STRING",
+    asn1.UTCTime: "UTCTIME",
+    asn1.Enumerated: "ENUMERATED",
+    asn1.Sequence: "SEQUENCE",
+    asn1.Set: "SET"
+}
+
+class_id_to_string_map = {
+    asn1.ClassUniversal: "U",
+    asn1.ClassApplication: "A",
+    asn1.ClassContext: "C",
+    asn1.ClassPrivate: "P"
+}
+
+object_id_to_string_map = {
+    "1.2.840.113549.1.1.1": "rsaEncryption",
+    "1.2.840.113549.1.1.5": "sha1WithRSAEncryption",
+
+    "1.3.6.1.5.5.7.1.1": "authorityInfoAccess",
+
+    "2.5.4.3": "commonName",
+    "2.5.4.4": "surname",
+    "2.5.4.5": "serialNumber",
+    "2.5.4.6": "countryName",
+    "2.5.4.7": "localityName",
+    "2.5.4.8": "stateOrProvinceName",
+    "2.5.4.9": "streetAddress",
+    "2.5.4.10": "organizationName",
+    "2.5.4.11": "organizationalUnitName",
+    "2.5.4.12": "title",
+    "2.5.4.13": "description",
+    "2.5.4.42": "givenName",
+
+    "1.2.840.113549.1.9.1": "emailAddress",
+
+    "2.5.29.14": "X509v3 Subject Key Identifier",
+    "2.5.29.15": "X509v3 Key Usage",
+    "2.5.29.16": "X509v3 Private Key Usage Period",
+    "2.5.29.17": "X509v3 Subject Alternative Name",
+    "2.5.29.18": "X509v3 Issuer Alternative Name",
+    "2.5.29.19": "X509v3 Basic Constraints",
+    "2.5.29.30": "X509v3 Name Constraints",
+    "2.5.29.31": "X509v3 CRL Distribution Points",
+    "2.5.29.32": "X509v3 Certificate Policies Extension",
+    "2.5.29.33": "X509v3 Policy Mappings",
+    "2.5.29.35": "X509v3 Authority Key Identifier",
+    "2.5.29.36": "X509v3 Policy Constraints",
+    "2.5.29.37": "X509v3 Extended Key Usage"
+}
+
+
+def tag_id_to_string(identifier):
     """Return a string representation of a ASN.1 id."""
-    if id == asn1.Boolean:
-        s = 'BOOLEAN'
-    elif id == asn1.Integer:
-        s = 'INTEGER'
-    elif id == asn1.OctetString:
-        s = 'OCTET STRING'
-    elif id == asn1.Null:
-        s = 'NULL'
-    elif id == asn1.ObjectIdentifier:
-        s = 'OBJECT IDENTIFIER'
-    elif id == asn1.Enumerated:
-        s = 'ENUMERATED'
-    elif id == asn1.Sequence:
-        s = 'SEQUENCE'
-    elif id == asn1.Set:
-        s = 'SET'
-    elif id == asn1.Null:
-        s = 'NULL'
-    else:
-        s = '%#02x' % id
-    return s
- 
-def strclass(id):
+    if identifier in tag_id_to_string_map:
+        return tag_id_to_string_map[identifier]
+    return '{:#02x}'.format(identifier)
+
+
+def class_id_to_string(identifier):
     """Return a string representation of an ASN.1 class."""
-    if id == asn1.ClassUniversal:
-        s = 'UNIVERSAL'
-    elif id == asn1.ClassApplication:
-        s = 'APPLICATION'
-    elif id == asn1.ClassContext:
-        s = 'CONTEXT'
-    elif id == san1.ClassPrivate:
-        s = 'PRIVATE'
+    if identifier in class_id_to_string_map:
+        return class_id_to_string_map[identifier]
+    raise ValueError('Illegal class: {:#02x}'.format(identifier))
+
+
+def object_identifier_to_string(identifier):
+    if identifier in object_id_to_string_map:
+        return object_id_to_string_map[identifier]
+    return identifier
+
+
+def value_to_string(tag_number, value):
+    if isinstance(value, bytes):
+        return '0x' + str(binascii.hexlify(value).upper())
+    elif isinstance(value, str):
+        return value
+    elif tag_number == asn1.ObjectIdentifier:
+        return object_identifier_to_string(value)
     else:
-        raise ValueError, 'Illegal class: %#02x' % id
-    return s
+        return repr(value)
 
-def strtag(tag):
-    """Return a string represenation of an ASN.1 tag."""
-    return '[%s] %s' % (strid(tag[0]), strclass(tag[2]))
 
-def prettyprint(input, output, indent=0):
+def pretty_print(input_stream, output_stream, indent=0):
     """Pretty print ASN.1 data."""
-    while not input.eof():
-        tag = input.peek()
-        if tag[1] == asn1.TypePrimitive:
-            tag, value = input.read()
-            output.write(' ' * indent)
-            output.write('[%s] %s (value %s)' %
-                         (strclass(tag[2]), strid(tag[0]), repr(value)))
-            output.write('\n')
-        elif tag[1] == asn1.TypeConstructed:
-            output.write(' ' * indent)
-            output.write('[%s] %s:\n' % (strclass(tag[2]), strid(tag[0])))
-            input.enter()
-            prettyprint(input, output, indent+2)
-            input.leave()
+    while not input_stream.eof():
+        tag = input_stream.peek()
+        if tag.typ == asn1.TypePrimitive:
+            tag, value = input_stream.read()
+            output_stream.write(' ' * indent)
+            output_stream.write('[{}] {}: {}\n'.format(class_id_to_string(tag.cls), tag_id_to_string(tag.nr), value_to_string(tag.nr, value)))
+        elif tag.typ == asn1.TypeConstructed:
+            output_stream.write(' ' * indent)
+            output_stream.write('[{}] {}\n'.format(class_id_to_string(tag.cls), tag_id_to_string(tag.nr)))
+            input_stream.enter()
+            pretty_print(input_stream, output_stream, indent + 2)
+            input_stream.leave()
 
 
 # Main script
 
 parser = optparse.OptionParser()
-parser.add_option('-p', '--pem', dest='mode', action='store_const',
-                  const='pem', help='PEM encoded input')
-parser.add_option('-r', '--raw', dest='mode', action='store_const',
-                  const='raw', help='raw input')
-parser.add_option('-o', '--output', dest='output',
-                  help='output to FILE instead', metavar='FILE')
+parser.add_option('-p', '--pem', dest='mode', action='store_const', const='pem', help='PEM encoded input')
+parser.add_option('-r', '--raw', dest='mode', action='store_const', const='raw', help='raw input')
+parser.add_option('-o', '--output', dest='output', help='output to FILE instead', metavar='FILE')
 parser.set_default('mode', 'pem')
 (opts, args) = parser.parse_args()
 
-if len(args) == 1:
-    input = file(sys.argv[1])
-else:
-    input = sys.stdin
-
 if opts.mode == 'pem':
-    input = read_pem(input)
+    input_file = open(sys.argv[1], 'r')
+    input_data = read_pem(input_file)
 else:
-    input = input.read()
+    input_file = open(sys.argv[1], 'rb')
+    input_data = input_file.read()
 
 if opts.output:
-    output = file(opts.output, 'w')
+    output_file = open(opts.output, 'w')
 else:
-    output = sys.stdout
+    output_file = sys.stdout
 
-data = []
-for line in input:
-    data.append(line)
-data = ''.join(data)
+decoder = asn1.Decoder()
+decoder.start(input_data)
 
-dec = asn1.Decoder()
-dec.start(data)
-
-prettyprint(dec, output)
+pretty_print(decoder, output_file)
